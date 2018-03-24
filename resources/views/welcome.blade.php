b6462b26b1eb2e0d67cfaa03bef09ea5c15716f8
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-03-25 02:29:27 +0600

    Welcome blade issue fixed

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+</head>
+<body>
+	<h1 style="text-align: center;">
+		Welcome to blog now this is on construction mode
+	</h1>
+</body>
+</html>
