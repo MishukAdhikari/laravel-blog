9c984524b03762e8d3d31a7afd5b4908c0acb445
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-06-13 14:35:01 +0600

    Sidebar seperated from template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,7 +21,25 @@
 
     @include('layouts.nav')
 
-    @yield('content')
+    <div class="blog-header">
+      <div class="container">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="row">
+
+        @yield('content')
+
+        @include('layouts.sidebar')
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
 
     @include('layouts.footer')
 
