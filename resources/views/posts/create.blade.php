a606499d381ed0af3bea85696fbfbedc9645aa34
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-06-17 17:18:17 +0600

    Html 5 validation and server validation added

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,25 +14,15 @@
 
 			<div class="form-group">
 				<label for="title">Title:</label>
-				<input type="title" class="form-control" id="title" name="title">
+				<input type="title" class="form-control" id="title" name="title" required>
 			</div>
 
 			<div class="form-group">
 				<label for="body">Body:</label>
-				<textarea class="form-control" id="body" name="body"></textarea>
+				<textarea class="form-control" id="body" name="body" required></textarea>
 			</div>
 
-			@if(count($errors))
-				<div class="form-group">
-					<div class="alert alert-danger">
-						<ul>
-							@foreach ($errors->all() as $error)
-								<li>{{ $error }}</li>
-							@endforeach
-						</ul>
-					</div>
-				</div>
-			@endif
+			@include('layouts.errors')
 
 			<button type="submit" class="btn btn-primary">Publish</button>
 
