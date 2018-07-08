89459977bfb40e347aa61c6a7cc15a370797060c
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-07-09 01:03:36 +0600

    Blog post seperated

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,7 +2,7 @@
 
 	<div class="blog-post">
 		<h2 class="blog-post-title">{{ $post->title }}</h2>
-		<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+		<p class="blog-post-meta">{{ $post->created_at->toDateTimeString() }}</p>
 
 		<p>
 		  {{ $post->body }}
