af2c469f7e43bbe4670b9119c5543d43e940e107
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-07-10 23:51:19 +0600

    Latest post filter added

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostsController extends Controller
 {
 	public function index()
 	{
-		$posts = Posts::all();
+		$posts = Posts::latest()->get();
 
 		return view( 'posts.index', compact('posts') );
 	}
