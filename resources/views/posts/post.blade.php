a1b2710d4e7c3d8ff6b281726967fd444680eae7
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-07-11 00:15:46 +0600

    Date and time fixed with carbon

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,7 +2,7 @@
 
 	<div class="blog-post">
 		<h2 class="blog-post-title">{{ $post->title }}</h2>
-		<p class="blog-post-meta">{{ $post->created_at->toDateTimeString() }}</p>
+		<p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
 
 		<p>
 		  {{ $post->body }}
